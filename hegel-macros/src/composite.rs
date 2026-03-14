44a0dd4fdf72b46684d0691a0f3f0514894a6eb1
River Keefer <river@k-monk.org>
2026-03-14 23:47:34 +0000

    Work on composite macro
    + Expect test case argument in first position
    + Passthrough parameters
    + Better error messages
    + Reuse existing compose macro under the hood

---
+++ hegel-macros/src/composite.rs
@@ -0,0 +1,71 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse2, FnArg, ItemFn, ReturnType, Type};
+use syn::token::Comma;
+use syn::punctuated::Punctuated;
+
+const MISSING_TEST_CASE_PARAMETER: &str = "Functions marked with #[hegel::composite] must have a TestCase parameter as their first argument.";
+const MISSING_COMPOSITE_RETURN_TYPE: &str = "Functions marked with #[hegel::composite] must have an explicit return type.";
+
+// Our goal is to expand this
+//
+// #[hegel::composite]
+// fn composite_generator(tc: TestCase, a: A, b: B) -> C {
+//     body
+// }
+//
+// into this
+//
+// fn composite_generator(a: A, b: B) -> ComposedGenerator<C, impl Fn(TestCase) -> C> {
+//     compose!(|tc| { body })
+// }
+
+pub fn expand_composite(
+    f: ItemFn,
+) -> TokenStream {
+
+    // Clone the input parameters into a vector, so we can pull the first one out.
+    let input_parameters: Vec<FnArg> = f.sig.inputs.iter().cloned().collect();
+
+    let Some((FnArg::Typed(tc_arg), passthrough)) = input_parameters.split_first() else {
+        panic!("{}", MISSING_TEST_CASE_PARAMETER)
+    };
+    let tc_pattern = &tc_arg.pat;
+    let tc_type = &tc_arg.ty;
+
+    // Check if the type path for the first parameter ends with a "TestCase" identifier. As far as
+    // I can tell, there's no good way of checking at procedural macro expansion time whether this
+    // actually resolves to the right type.
+    let Type::Path(path) = tc_type.as_ref() else {
+        panic!("{}", MISSING_TEST_CASE_PARAMETER)
+    };
+    match path.path.segments.last() {
+        Some(last) if last.ident == "TestCase" => (),
+        _ => panic!("{}", MISSING_TEST_CASE_PARAMETER),
+    };
+
+    let ReturnType::Type(_, return_type) = &f.sig.output else {
+        panic!("{}", MISSING_COMPOSITE_RETURN_TYPE)
+    };
+
+    let composed_generator_type = quote! {
+        -> ::hegel::generators::ComposedGenerator<#return_type, impl Fn(::hegel::TestCase) -> #return_type>
+    };
+
+    let mut sig = f.sig;
+    sig.output = parse2(composed_generator_type).unwrap();
+    sig.inputs = passthrough
+        .iter()
+        .cloned()
+        .collect::<Punctuated<FnArg, Comma>>();
+
+    let body = &f.block;
+    let attrs = &f.attrs;
+    let vis = &f.vis;
+
+    quote! {
+        #(#attrs)*
+        #vis #sig
+        { ::hegel::compose!(|#tc_pattern| #body) }
+    }
+}
--- tests/test_composite.rs
+++ tests/test_composite.rs
@@ -2,13 +2,13 @@ use hegel::generators::integers;
 use hegel::TestCase;
 
 #[hegel::composite]
-fn composite_integer_generator(tc: TestCase) -> i32 {
+fn composite_integer_generator(tc: TestCase, n: i32) -> i32 {
     let x = tc.draw(integers::<i32>().min_value(0).max_value(100));
-    x + 1
+    x + n
 }
 
 #[hegel::test]
 fn test_composite_generation(tc: TestCase) {
-    let x = tc.draw(composite_integer_generator());
+    let x = tc.draw(composite_integer_generator(1));
     assert!(x > 0);
 }
