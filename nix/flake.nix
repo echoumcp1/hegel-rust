686313f58b39f5be8100c121e19a14530018880c
Winter M <winter@antithesis.com>
2026-03-23 14:57:30 -0400

    nix: use `lib.systems.flakeExposed`

--- nix/flake.nix
+++ nix/flake.nix
@@ -16,14 +16,7 @@
       ...
     }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
       devShells = forAllSystems (
